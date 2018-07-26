958bfaf36c397413490bb25081bfb005685aa83a
donluispanis <lga42@gcloud.ua.es>
2018-07-26 16:48:22 +0200

    Paint tool finished :smile:

--- src/Tools.cpp
+++ src/Tools.cpp
@@ -19,10 +19,9 @@ void Pencil::OnClick(const point_t p, Canvas &canvas)
 
             oldP = p;
         }
-        else
+        else if (p.x != oldP.x || p.y != oldP.y)
         {
-            point_t p1 = {300, 300};
-            oldP = {400, 300};
+            point_t p1 = p;
             float slope = (float)(p1.y - oldP.y) / (float)(p1.x - oldP.x);
             float acummulated = 0.f;
             point_t aux = oldP;
@@ -80,10 +79,52 @@ void Pencil::OnClick(const point_t p, Canvas &canvas)
             //Descending slope < 45º
             else if (slope >= -1.f && slope < 0.f)
             {
+                while (aux.x != p1.x || aux.y != p1.y)
+                {
+                    int sD = ((canvas.getHeight() - aux.y) * canvas.getWidth() + aux.x) * 4;
+
+                    canvas.getCanvasData()[sD] = getColor().RGBA[0];
+                    canvas.getCanvasData()[sD + 1] = getColor().RGBA[1];
+                    canvas.getCanvasData()[sD + 2] = getColor().RGBA[2];
+                    canvas.getCanvasData()[sD + 3] = getColor().RGBA[3];
+
+                    int signX = (p1.x - aux.x) >= 0 ? 1 : -1;
+
+                    aux.x += signX;
+                    acummulated += slope;
+
+                    if (acummulated <= -1.f)
+                    {
+                        int signY = (p1.y - aux.y) >= 0 ? 1 : -1;
+                        aux.y += signY;
+                        acummulated += 1.f;
+                    }
+                }
             }
             //Descending slope > 45º
             else if (slope < -1.f)
             {
+                while (aux.x != p1.x || aux.y != p1.y)
+                {
+                    int sD = ((canvas.getHeight() - aux.y) * canvas.getWidth() + aux.x) * 4;
+
+                    canvas.getCanvasData()[sD] = getColor().RGBA[0];
+                    canvas.getCanvasData()[sD + 1] = getColor().RGBA[1];
+                    canvas.getCanvasData()[sD + 2] = getColor().RGBA[2];
+                    canvas.getCanvasData()[sD + 3] = getColor().RGBA[3];
+
+                    int signY = (p1.y - aux.y) >= 0 ? 1 : -1;
+
+                    aux.y += signY;
+                    acummulated += 1 / slope;
+
+                    if (acummulated <= -1.f)
+                    {
+                        int signX = (p1.x - aux.x) >= 0 ? 1 : -1;
+                        aux.x += signX;
+                        acummulated += 1.f;
+                    }
+                }
             }
 
             oldP = p;
