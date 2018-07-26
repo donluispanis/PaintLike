c39adb09f99b1f4e41aaf728fdd4e8f86a31eef7
donluispanis <lga42@gcloud.ua.es>
2018-07-26 12:04:26 +0200

    Canvas added :cake:

--- src/Tools.cpp
+++ src/Tools.cpp
@@ -4,14 +4,17 @@
 
 void Pencil::OnClick(int x, int y, Canvas &canvas)
 {
-    int sD = ((canvas.getHeight() - y) * canvas.getWidth() + x) * 4;
+    if (x >= 0 && x < canvas.getWidth() && y >= 0 && y < canvas.getHeight())
+    {
+        int sD = ((canvas.getHeight() - y) * canvas.getWidth() + x) * 4;
 
-    canvas.getCanvasData()[sD] = getColor().RGBA[0];
-    canvas.getCanvasData()[sD + 1] = getColor().RGBA[1];
-    canvas.getCanvasData()[sD + 2] = getColor().RGBA[2];
-    canvas.getCanvasData()[sD + 3] = getColor().RGBA[3];
+        canvas.getCanvasData()[sD] = getColor().RGBA[0];
+        canvas.getCanvasData()[sD + 1] = getColor().RGBA[1];
+        canvas.getCanvasData()[sD + 2] = getColor().RGBA[2];
+        canvas.getCanvasData()[sD + 3] = getColor().RGBA[3];
 
-    isPressed = true;
+        isPressed = true;
+    }
 }
 
 void Pencil::OnRelease(int x, int y, Canvas &canvas)
